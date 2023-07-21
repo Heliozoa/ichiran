540fdacc3e2ec8319eb7e2baa9c0d1eff007f0c9
Heliozoa <daniel.x.martinez@helsinki.fi>
2023-07-21 18:32:49 +0300

    Add settings.lisp

---
+++ settings.lisp
@@ -0,0 +1,17 @@
+(in-package #:ichiran/conn)
+
+(defparameter *connection* '("ichiran" "lbr" "lbr" "localhost"))
+
+(defparameter *connections* '((:old "jmdict_old" "postgres" "password" "localhost")
+                              (:test "jmdict_test" "postgres" "password" "localhost")))
+
+(in-package #:ichiran/dict)
+
+(defparameter *jmdict-path* #p"/home/you/dump/JMdict_e")
+
+(defparameter *jmdict-data* #p"/home/sasami-san/jmdictdb/jmdictdb/data/")
+(defparameter *jmdict-data* #p"/lbr/data/jmdictdb/")
+
+(in-package #:ichiran/kanji)
+
+(defparameter *kanjidic-path* #P"/home/you/dump/kanjidic2.xml")
